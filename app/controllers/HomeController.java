1e64d9beeead77f712584c841c04b69496869d20
Live session user <sharadhi.nmurthy@gmail.com>
2017-09-15 12:13:20 +0000

    Fourth Commit

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -8,7 +8,8 @@ import views.html.*;
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
-public class HomeController extends Controller {
+public class HomeController extends Controller 
+{
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -16,8 +17,9 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result index() {
-        return ok("Hello World");
+    public Result index() 
+    {
+        return ok(views.html.index.render()); //Views is the package, html is the template, index is a member in views, render () is a method called on this, with injected values
     }
 
 }
